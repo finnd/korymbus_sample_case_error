f90c7cb0b4be9d6e26febc55963ca94a3021ccf0
finnd <dmfinn@wustl.edu>
2017-07-27 12:33:06 -0400

    Update samplecase.py

---
+++ samplecase.py
@@ -0,0 +1,17 @@
+
+import os
+import sys
+
+
+def printTime():
+    print("Time is only a concept")
+
+def acceptThreeArgs(arg1, arg2, arg3):
+    return "thanks for the three args"
+
+
+if __name__ == '__main__':
+    eeeee
+    printTim()
+    acceptThreeArgs(arg1,arg2)
+API Training Shop Blog About
